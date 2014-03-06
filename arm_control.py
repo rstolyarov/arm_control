43156f2766588da8f5ceeafe64da93855b165623
Roman Stolyarov <rstolyarov@smu.edu>
2014-03-06 05:58:14 +0000

    added some threading to motor control

--- arm_control.py
+++ arm_control.py
@@ -23,31 +23,34 @@ def sampleTouch():
   pass
 
 def incrementMotor(servo, i, intensity, factor):
-  motor_id = 0
-  curPos = 0
+  def threadIncrementMotor(servo, i, intensity,factor):
+    motor_id = 0
+    curPos = 0
   
-  if i==0 or i==1: 
-    motor_id = CLAW_OUT 
-    curPos = clawPos
-  if i==2 or i==3: 
-    motor_id = WRIST_OUT
-    curPos = wristPos
-  if i==4 or i==5: 
-    motor_id = ELBOW_OUT
-    curPos = elbowPos
+    if i==0 or i==1: 
+      motor_id = CLAW_OUT 
+      curPos = clawPos
+    if i==2 or i==3: 
+      motor_id = WRIST_OUT
+      curPos = wristPos
+    if i==4 or i==5: 
+      motor_id = ELBOW_OUT
+      curPos = elbowPos
   
-  increment = intensity * factor
-  newPos = 0
+    increment = intensity * factor
+    newPos = 0
   
-  if (i%2==0): 
-    newPos = curPos + increment
-  else: 
-    newPos = curPos - increment
-  servo.set_servo(motor_id, newPos)
+    if (i%2==0): 
+      newPos = curPos + increment
+    else: 
+      newPos = curPos - increment
+    servo.set_servo(motor_id, newPos)
   
-  if i==0 or i==1: clawPos = newPos 
-  if i==2 or i==3: wristPos = newPos 
-  if i==4 or i==5: elbowPos = newPos
+    if i==0 or i==1: clawPos = newPos 
+    if i==2 or i==3: wristPos = newPos 
+    if i==4 or i==5: elbowPos = newPos
+  t = threading.Thread(target=threadIncrementMotor, args=(servo,i,intensity,factor))
+  t.start()
 
 def lightOn():
   print "light on"
